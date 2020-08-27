c308b580faf9099b8bbe871e9406095e06d42182
Paulo Victor <paulo.victor158@gmail.com>
2020-08-27 09:35:31 -0400

    adding necessary components to build cross-platform bottom navigation

--- lib/ui/view/movies_list_screen/movies_list_screen.dart
+++ lib/ui/view/movies_list_screen/movies_list_screen.dart
@@ -1,11 +1,9 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 
 import '../../../data/dio/dio_client.dart';
 import '../../../data/model/model.dart';
 
-import 'components/movies_list.dart';
+import 'components/movies_content_body.dart';
 
 class MoviesListScreen extends StatefulWidget {
   @override
@@ -47,78 +45,28 @@ class _MoviesListScreenState extends State<MoviesListScreen> {
 
   @override
   Widget build(BuildContext context) => Scaffold(
-          body: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            expandedHeight: 250,
-            flexibleSpace: FlexibleSpaceBar(
-              title: const Text('TMDb'),
-              centerTitle: true,
-              background: Padding(
-                padding: const EdgeInsets.all(20),
-                child: Image.asset('lib/ui/assets/top-20.png'),
+        body: CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              expandedHeight: 250,
+              flexibleSpace: FlexibleSpaceBar(
+                title: const Text('TMDb'),
+                centerTitle: true,
+                background: Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Image.asset('lib/ui/assets/top-20.png'),
+                ),
               ),
             ),
-          ),
-          _MoviesListBody(
-            error: _error,
-            moviesList: _moviesList,
-            awaitingMoviesList: _awaitingMoviesList,
-            onTryAgainTap: () {
-              _getMovies();
-            },
-          ),
-        ],
-      ));
-}
-
-class _MoviesListBody extends StatelessWidget {
-  const _MoviesListBody({
-    @required this.awaitingMoviesList,
-    @required this.onTryAgainTap,
-    this.error,
-    this.moviesList,
-  })  : assert(awaitingMoviesList != null),
-        assert(onTryAgainTap != null);
-
-  final dynamic error;
-  final List<MovieShortDetails> moviesList;
-  final bool awaitingMoviesList;
-  final VoidCallback onTryAgainTap;
-
-  @override
-  Widget build(BuildContext context) {
-    if (awaitingMoviesList) {
-      return const SliverFillRemaining(
-        child: Center(
-          child: CircularProgressIndicator(),
-        ),
-      );
-    } else if (error != null) {
-      return SliverFillRemaining(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const SizedBox(
-              height: 10,
-            ),
-            if (error is SocketException)
-              const Text(
-                'Verifique sua conexão',
-                style: TextStyle(
-                    color: Colors.red,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 12),
-              ),
-            RaisedButton(
-              onPressed: onTryAgainTap,
-              child: const Text('Tentar Novamente'),
+            MoviesContentBody(
+              error: _error,
+              moviesList: _moviesList,
+              awaitingMoviesList: _awaitingMoviesList,
+              onTryAgainTap: () {
+                _getMovies();
+              },
             ),
           ],
         ),
       );
-    } else {
-      return MoviesList(moviesList: moviesList);
-    }
-  }
 }
