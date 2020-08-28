90f1719cd1ae810c3eaf75d408b6b41d10620e7a
Paulo Victor <paulo.victor158@gmail.com>
2020-08-28 14:38:17 -0400

    updating branch

---
+++ lib/ui/components/page_navigation.dart
@@ -0,0 +1,35 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+import '../../ui/view/movie_details_screen/movie_details_screen.dart';
+
+PageRoute<T> _buildAdaptivePageRoute<T>({
+  @required WidgetBuilder builder,
+  bool fullscreenDialog = false,
+  Object arguments,
+}) =>
+    Platform.isAndroid
+        ? MaterialPageRoute(
+            settings: RouteSettings(arguments: arguments),
+            builder: builder,
+            fullscreenDialog: fullscreenDialog,
+          )
+        : CupertinoPageRoute(
+            settings: RouteSettings(arguments: arguments),
+            builder: builder,
+            fullscreenDialog: fullscreenDialog,
+          );
+
+void pushPage(BuildContext context, bool isHorizontalNavigation,
+    {Object arguments}) {
+  Navigator.of(context, rootNavigator: !isHorizontalNavigation).push(
+    _buildAdaptivePageRoute(
+      builder: (context) => MovieDetailsScreen(),
+      fullscreenDialog: !isHorizontalNavigation,
+      arguments: arguments,
+    ),
+  );
+}
