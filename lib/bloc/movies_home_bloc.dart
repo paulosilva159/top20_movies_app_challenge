d5807c35a739549ce33029a5a7de3bf0199e2864
Paulo Victor <paulo.victor158@gmail.com>
2020-09-04 13:26:47 -0400

    reviewing code before PR 2

--- lib/bloc/movies_home_bloc.dart
+++
@@ -1,5 +0,0 @@
-// TODO(Paulo): Implementar o _userLocale como sendo input
-// TODO(Paulo): Implementar o _appFlow como sendo output
-// TODO(Paulo): Atualizar o _appFlow toda vez que o _userLocale mudart
-
-class MoviesHomeBloc {}
